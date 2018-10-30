a46f857fb9242396a74ab6b3a0442b1e8de30335
Joshua Westerheide <dev@jdoubleu.de>
2018-10-30 15:59:36 +0100

    Install bootstrap-vue design system

---
+++ src/renderer/plugins/design.js
@@ -0,0 +1,8 @@
+/* Initialize Design System: Bootstrap */
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue);
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
