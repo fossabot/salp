f460417515a9f5def691e122d8974bf7232350ab
Joshua Westerheide <dev@jdoubleu.de>
2018-11-15 14:02:32 +0100

    Change size of window

--- src/main/index.js
+++ src/main/index.js
@@ -20,7 +20,10 @@ let mainWindow
 // Standard scheme must be registered before the app is ready
 protocol.registerStandardSchemes(['app'], { secure: true })
 function createMainWindow () {
-  const window = new BrowserWindow()
+  const window = new BrowserWindow({
+    width: 1440,
+    height: 960
+  })
   
   setupLog()
 
