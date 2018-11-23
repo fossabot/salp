5f05d4dec3b0862feae20496d6636a9c2762fe6b
Joshua Westerheide <dev@jdoubleu.de>
2018-11-23 14:23:16 +0100

    Rework fontawesome icon integration
    Add custom Icon component and icon plugin for on-demand including of icons.
    
    Remove global stylesheet.

---
+++ src/renderer/components/Elements/Icon.vue
@@ -0,0 +1,22 @@
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+export default {
+    name: 'Icon',
+    functional: true,
+    render(createElement, context) {
+        const { props, parent } = context
+
+        // Check if icon is string and should use from registered icons in parent
+        if (props.icon && typeof props.icon === 'string' 
+            && parent.$icons && parent.$icons[props.icon]) {
+                props.icon = parent.$icons[props.icon]
+        }
+
+        return createElement(FontAwesomeIcon, {
+            props: context.props,
+            staticClass: 'el-icon-fa'
+        })
+    }
+}
+</script>
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -4,6 +4,7 @@ import router from './plugins/router'
 import store from './plugins/store'
 import i18n from './plugins/i18n'
 import './plugins/design'
+import './plugins/icons'
 
 Vue.config.productionTip = false
 
