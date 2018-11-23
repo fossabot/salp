360dccfd7008b73cdda1290595de645efa05fe5d
Joshua Westerheide <dev@jdoubleu.de>
2018-11-23 03:30:06 +0100

    Fix tests failing if they include element-ui components

--- babel.config.js
+++ babel.config.js
@@ -1,31 +1,14 @@
 const path = require('path')
 const fileExists = require('fs').existsSync
 
-module.exports = {
+const isTesting = process.env.NODE_ENV === 'test'
+
+const babelConfig = {
   presets: [
     '@vue/app'
   ],
   plugins: [
-    'lodash',
-    [
-      // Use forked babel plugin for component based includes
-      // @url https://github.com/ant-design/babel-plugin-import
-      // Note: After removing or adding new files to the theme, the babel-loader cache has to be flushed
-      'import',
-      {
-        'libraryName': 'element-ui',
-        'style': name => {
-          const component = name.split('/').pop()
-          let stylePath = path.resolve(__dirname, `src/renderer/theme/element/${component}.scss`)
-
-          if (!fileExists(stylePath)) {
-            stylePath = `element-ui/packages/theme-chalk/src/${component}.scss`
-          }
-
-          return stylePath
-        }
-      }
-    ]
+    'lodash'
   ],
   env: {
     'test': {
@@ -35,3 +18,27 @@ module.exports = {
     }
   }
 }
+
+if (!isTesting) {
+  babelConfig.plugins.push([
+    // Use forked babel plugin for component based includes
+    // @url https://github.com/ant-design/babel-plugin-import
+    // Note: After removing or adding new files to the theme, the babel-loader cache has to be flushed
+    'import',
+    {
+      'libraryName': 'element-ui',
+      'style': name => {
+        const component = name.split('/').pop()
+        let stylePath = path.resolve(__dirname, `src/renderer/theme/element/${component}.scss`)
+
+        if (!fileExists(stylePath)) {
+          stylePath = `element-ui/packages/theme-chalk/src/${component}.scss`
+        }
+
+        return stylePath
+      }
+    }
+  ])
+}
+
+module.exports = babelConfig
