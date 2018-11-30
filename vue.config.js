d8c0dee02f9c5dc194f64f9dd018f3b4902a0673
Joshua Westerheide <dev@jdoubleu.de>
2018-11-30 15:21:19 +0100

    Install and configure code coverage for renderer unit tests

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,21 @@ module.exports = {
 
         config.plugins.delete('preload')
         config.plugins.delete('prefetch')    
+
+        // Code coverage
+        if (process.env.NODE_ENV === 'test') {
+            config.module.rule('js')
+                .use('istanbul')
+                    .loader('istanbul-instrumenter-loader')
+                    .options({ esModules: true })
+                    .before('babel-loader')
+
+            config.output
+                .devtoolModuleFilenameTemplate('[absolute-resource-path]')
+                .devtoolFallbackModuleFilenameTemplate('[absolute-resource-path]?[hash]')
+
+            config.devtool('inline-cheap-module-source-map')
+        }
     },
     pluginOptions: {
         electronBuilder: {
