7828813974f6185cda4230d421118efb19526bdf
Joshua Westerheide <dev@jdoubleu.de>
2018-12-01 16:26:39 +0100

    Update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,42 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-    mocha: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+    root: true,
+    env: {
+        node: true,
+        mocha: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'indent': [
+            'error',
+            4
+        ],
+        'new-cap': [
+            'error',
+            {
+                'newIsCapExceptions': [
+                    'createLocalVue'
+                ]
+            }
+        ],
+        'space-before-function-paren': [
+            'error',
+            {
+                'anonymous': 'never',
+                'named': 'never',
+                'asyncArrow': 'always'
+            }
+        ],
+        'operator-linebreak': [
+            'error',
+            'before'
+        ]
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
 }
