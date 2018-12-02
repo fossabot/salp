efa3f8e31a1968620cc1289250cf86f4e59fe5e8
Max van Aerssen <max@vanaerssen.de>
2018-12-02 19:22:44 +0100

    Add code component

---
+++ src/renderer/components/Elements/Code.spec.js
@@ -0,0 +1,31 @@
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import Code from './Code.vue'
+
+describe('Code.vue', () => {
+    it('highlightedLines validator only allows valid ranges', () => {
+        const expectedValues = [
+            { value: [], expects: true },
+            { value: [1], expects: true },
+            { value: [1, 5, 7], expects: true },
+            { value: [[1, 2]], expects: true },
+            { value: [[1]], expects: false },
+            { value: [1, [1, 3], 4], expects: true },
+            { value: [1, [1], 4], expects: false },
+            { value: [-1], expects: false },
+            { value: [0], expects: false },
+            { value: [5, 5], expects: true },
+            { value: [{}], expects: false },
+            { value: ['1'], expects: false },
+            { value: 5, expects: false },
+            { value: [[7,2]], expects: false },
+            { value: [7.5], expects: false }
+        ]
+
+        const validator = Code.props.highlightedLines.validator
+
+        expectedValues.forEach(({ value, expects }) => {
+            expect(validator(value)).to.equal(expects, `validating ${value} to be ${expects}`)
+        })
+    })
+})
