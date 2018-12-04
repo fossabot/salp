581497de4debf34fdf728a8946e6615c5540657b
Joshua Westerheide <dev@jdoubleu.de>
2018-12-04 18:32:27 +0100

    Add TableOfContents layout component
    Implement the main logic in a mixin.
    
    Also add tests.

---
+++ src/renderer/components/Layout/Content/TableOfContents.vue
@@ -0,0 +1,30 @@
+<script>
+import TableOfContentsMixin from '@/mixins/TableOfContents.js'
+
+export default {
+    name: 'TableOfContents',
+    mixins: [
+        TableOfContentsMixin
+    ],
+    methods: {
+        createRootElement(toc) {
+            return this.$createElement('nav', {
+                staticClass: 'toc toc--layout-content'
+            }, [toc])
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.toc--layout-content {
+    .toc__menu {
+        list-style-type: decimal;
+    }
+
+    .toc__link {
+        display: block;
+        padding: 0.5em 0;
+    }
+}
+</style>
