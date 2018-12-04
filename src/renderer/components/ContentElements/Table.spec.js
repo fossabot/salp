07761cf1e2002884654ce28771fc11e8ebf7c386
Max van Aerssen <max@vanaerssen.de>
2018-12-04 18:54:26 +0100

    Add table component

---
+++ src/renderer/components/ContentElements/Table.spec.js
@@ -0,0 +1,41 @@
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import Table from './Table.vue'
+
+describe('Table.vue', () => {
+    let wrapper = {}
+
+    beforeEach(() => {
+        wrapper = shallowMount(Table, {
+            context: {
+                props: {
+                    data: [{
+                        date: '2016-05-03',
+                        name: 'Tom',
+                        address: 'No. 189, Grove St, Los Angeles'
+                    }, {
+                        date: '2016-05-02',
+                        name: 'Tom',
+                        address: 'No. 189, Grove St, Los Angeles'
+                    }, {
+                        date: '2016-05-04',
+                        name: 'Tom',
+                        address: 'No. 189, Grove St, Los Angeles'
+                    }, {
+                        date: '2016-05-01',
+                        name: 'Tom',
+                        address: 'No. 189, Grove St, Los Angeles'
+                    }]
+                }
+            }
+        })
+    })
+
+    it('class contentelement-table should be added', () => {
+        expect(wrapper.classes('contentelement-table')).to.be.true
+    })
+
+    it('table tag should be created', () => {
+        expect(wrapper.contains('table')).to.be.true
+    })
+})
