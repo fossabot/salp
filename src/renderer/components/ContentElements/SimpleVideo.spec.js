596d260e3e4746352425acebf87910c2e4e9afb8
Max van Aerssen <max@vanaerssen.de>
2018-12-04 18:53:36 +0100

    Add simpleVideo component

---
+++ src/renderer/components/ContentElements/SimpleVideo.spec.js
@@ -0,0 +1,38 @@
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import SimpleVideo from './SimpleVideo.vue'
+
+describe('SimpleVideo.vue', () => {
+    let wrapper = {}
+    const expectedSrc = 'path/to/video/file'
+
+    beforeEach(() => {
+        wrapper = shallowMount(SimpleVideo, {
+            context: {
+                props: {
+                    src: expectedSrc
+                }
+            }
+        })
+    })
+
+    it('class contentelement-video should be added', () => {
+        expect(wrapper.classes('contentelement-video')).to.be.true
+    })
+
+    it('controls should be enabled', () => {
+        expect(wrapper.classes('controls')).to.be.true
+    })
+
+    it('video tag should be created', () => {
+        expect(wrapper.contains('video')).to.be.true
+    })
+
+    it('source tag should be created', () => {
+        expect(wrapper.contains('source')).to.be.true
+    })
+
+    it('src should be set', () => {
+        expect(wrapper.find('source').attributes('src')).to.equal(expectedSrc)
+    })
+})
---
+++ src/renderer/components/ContentElements/SimpleVideo.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: 'SimpleVideo',
+    functional: true,
+    props: {
+        src: {
+            type: String,
+            required: true
+        }
+    },
+    render(createElement, context) {
+        const { props } = context
+
+        return createElement(
+            'video',
+            {
+                staticClass: `contentelement-video controls`
+            },
+            [
+                createElement(
+                    'source',
+                    {
+                        attrs: {
+                            src: props.src
+                        }
+                    }
+                )
+            ]
+        )
+    }
+}
+</script>
