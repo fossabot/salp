47b0ff7a4c5d89871a1578dafe70a84bbef829f9
Max van Aerssen <max@vanaerssen.de>
2018-12-05 01:03:55 +0100

    Add appPreview component

---
+++ src/renderer/components/ContentElements/AppPreview.spec.js
@@ -0,0 +1,48 @@
+import { expect } from 'chai'
+import { mount } from '@vue/test-utils'
+import AppPreview from './AppPreview.vue'
+import SimpleLink from './SimpleLink.vue'
+import SimpleImage from './SimpleImage.vue'
+import SimpleText from './SimpleText.vue'
+import Heading from './Heading.vue'
+
+describe('AppPreview.vue', () => {
+    let wrapper = {}
+    const expectedImageSrc = 'path/to/image/file'
+    const expectedHeading = 'Lorem Ipsum'
+    const expectedDescription = 'dolor sit amet'
+    const expectedUrl = 'testurl'
+
+    beforeEach(() => {
+        wrapper = mount(AppPreview, {
+            context: {
+                props: {
+                    imageSrc: expectedImageSrc,
+                    heading: expectedHeading,
+                    description: expectedDescription,
+                    url: expectedUrl
+                }
+            }
+        })
+    })
+
+    it('class contentelement-app-preview should be added', () => {
+        expect(wrapper.classes('contentelement-app-preview')).to.be.true
+    })
+
+    it('simpleImage component should be created', () => {
+        expect(wrapper.find(SimpleImage).exists()).to.be.true
+    })
+
+    it('heading component should be created', () => {
+        expect(wrapper.find(Heading).exists()).to.be.true
+    })
+
+    it('simpleText component should be created', () => {
+        expect(wrapper.find(SimpleText).exists()).to.be.true
+    })
+
+    it('simpleLink component should be created', () => {
+        expect(wrapper.find(SimpleLink).exists()).to.be.true
+    })
+})
---
+++ src/renderer/components/ContentElements/AppPreview.vue
@@ -0,0 +1,96 @@
+<script>
+import SimpleImage from './SimpleImage.vue'
+import Heading from './Heading.vue'
+import SimpleText from './SimpleText.vue'
+import SimpleLink from './SimpleLink.vue'
+import { Card } from 'element-ui'
+
+export default {
+    name: 'AppPreview',
+    functional: true,
+    components: {
+        SimpleImage,
+        Heading,
+        SimpleText,
+        SimpleLink,
+        Card
+    },
+    props: {
+        imageSrc: {
+            type: String,
+            default() {
+                return 'image/default'
+            }
+        },
+        heading: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            default() {
+                return ''
+            }
+        }
+    },
+    render(createElement, context) {
+        const { props } = context
+
+        return createElement(
+            Card,
+            {
+                staticClass: `contentelement-app-preview`
+            },
+            [
+                createElement(
+                    SimpleImage,
+                    {
+                        staticClass: `contentelement-app-preview-image`,
+                        props: {
+                            src: props.imageSrc
+                        }
+                    }
+                ),
+                createElement(
+                    'div',
+                    {
+                        staticClass: `contentelement-app-preview-content__wrapper`
+                    },
+                    [
+                        createElement(
+                            Heading,
+                            {
+                                staticClass: `contentelement-app-preview-content-heading`,
+                                props: {
+                                    level: 3
+                                }
+                            },
+                            props.heading
+                        ),
+                        createElement(
+                            SimpleText,
+                            {
+                                staticClass: `contentelement-app-preview-content-description`
+                            },
+                            props.description
+                        ),
+                        createElement(
+                            SimpleLink,
+                            {
+                                staticClass: `contentelement-app-preview-content-link`,
+                                props: {
+                                    url: props.url
+                                }
+                            }
+                        )
+                    ]
+                )
+            ]
+        )
+    }
+}
+</script>
