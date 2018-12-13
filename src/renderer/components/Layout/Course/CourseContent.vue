cb77b5e7e4bf1464a867e18b8c9df1cca2e16c27
Max van Aerssen <max@vanaerssen.de>
2018-12-13 17:52:44 +0100

    Add course content component

---
+++ src/renderer/components/Layout/Course/CourseContent.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="course-content__container">
+        <component :is="getCurrentLecture"></component>
+        <div class="course-content__container__pagination__wrapper">
+            <Pagination layout="prev, pager, next" :total="pages" :page-size="1" @current-change="handlePageChange"/>
+        </div>
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+import Heading from '../../ContentElements/Heading.vue'
+import SimpleImage from '../../ContentElements/SimpleImage.vue'
+import AdvancedImage from '../../ContentElements/AdvancedImage.vue'
+import SimpleLink from '../../ContentElements/SimpleLink.vue'
+import SimpleText from '../../ContentElements/SimpleText.vue'
+import AppPreview from '../../ContentElements/AppPreview.vue'
+import SimpleVideo from '../../ContentElements/SimpleVideo.vue'
+import Citation from '../../ContentElements/Citation.vue'
+import YouTube from '../../ContentElements/YouTube.vue'
+import Table from '../../ContentElements/Table.vue'
+import Code from '../../Elements/Code.vue'
+import Lecture1 from '@/__mocks__/lectures/lecture1.vue'
+import Lecture2 from '@/__mocks__/lectures/lecture2.vue'
+
+import { Pagination } from 'element-ui'
+
+export default {
+    name: 'CourseContent',
+    props: {
+        course: Object
+    },
+    components: {
+        Heading,
+        SimpleLink,
+        SimpleText,
+        SimpleImage,
+        AdvancedImage,
+        SimpleVideo,
+        AppPreview,
+        Citation,
+        YouTube,
+        Table,
+        Code,
+
+        Lecture1,
+        Lecture2,
+
+        Pagination
+    },
+    data() {
+        return {
+            lectures: [
+                'Lecture1',
+                'Lecture2'
+            ],
+            currentLecture: 0
+        }
+    },
+    computed : {
+        getCurrentLecture: function() {
+            return this.lectures[this.currentLecture]
+        },
+        pages: function() {
+            return this.lectures.length
+        }
+    },
+    methods: {
+        handlePageChange(page) {
+            this.currentLecture = page - 1
+        }
+    }
+}
+</script>
+<style lang="scss">
+.course-content__container__pagination__wrapper {
+    display: flex;
+    justify-content: center;
+    margin-top: 1em;
+}
+</style>
