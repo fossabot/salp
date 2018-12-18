cba062dcf26e003ccf6ad85e0dbb361f6b4975c5
Max van Aerssen <max@vanaerssen.de>
2018-12-18 20:01:32 +0100

    Add UserInput component

---
+++ src/renderer/components/TestElements/UserInput.spec.js
@@ -0,0 +1,43 @@
+import { expect } from 'chai'
+import { shallowMount, mount } from '@vue/test-utils'
+import UserInput from './UserInput.vue'
+
+describe('UserInput.vue', () => {
+    const question = 'Lorem ipsum dollor sit atmet?'
+
+    describe('Test positioning of the question', () => {
+        const expectedValues = [
+            { questionPosition: 'top', expects: 'top' },
+            { questionPosition: 'Top', expects: 'top' },
+            { questionPosition: 'left', expects: 'left' },
+            { questionPosition: 'Left', expects: 'left' }
+        ]
+
+        expectedValues.forEach(({ questionPosition, expects }, index) => {
+            it(`should have class:.${expects} for position:${questionPosition} `, () => {
+                const wrapper = shallowMount(UserInput, {
+                    propsData: {
+                        question,
+                        questionPosition,
+                        correctCallback: () => {}
+                    }
+                })
+
+                expect(wrapper.classes(expects)).to.be.true
+            })
+        })
+    })
+    it('should change the v-model correctly', () => {
+        let wrapper = mount({
+            data() { return { answer: '' } },
+            template: `<div> <user-input :question="'question'" v-model="answer"></user-input> </div>`,
+            components: { 'user-input': UserInput }
+        })
+
+        const expectedAnswer = 'lorem'
+        let userInputField = wrapper.find('.user-input__container__input > input')
+        userInputField.value = expectedAnswer
+        userInputField.trigger('input')
+        expect(wrapper.vm.answer).to.eq(expectedAnswer)
+    })
+})
---
+++ src/renderer/components/TestElements/UserInput.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="user-input__container" v-bind:class="{left: isLeftPosition, top: isTopPosition}">
+        <h1 class="user-input__container__text">{{ question }}</h1>
+        <Input class="user-input__container__input" :value="answer" @change="handleChange" placeholder="Insert Answer"/>
+    </div>
+</template>
+
+<script>
+import { Input } from 'element-ui'
+
+export default {
+    name: 'UserInput',
+    components: {
+        Input
+    },
+    model: {
+        prop: 'answer',
+        event: 'change'
+    },
+    props: {
+        question: {
+            type: String,
+            required: true
+        },
+        questionPosition: {
+            type: String,
+            default() {
+                return 'top'
+            }
+        },
+        answer: String
+    },
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+        isLeftPosition() {
+            return this.questionPosition === 'left' || this.questionPosition === 'Left'
+        },
+        isTopPosition() {
+            return this.questionPosition === 'top' || this.questionPosition === 'Top'
+        }
+    },
+    methods: {
+        handleChange(value) {
+            this.$emit('change', value)
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.user-input__container {
+    display: flex;
+
+    .user-input__container__input {
+        width: fit-content;
+        margin-left: 1em;
+    }
+}
+.left {
+    flex-direction: row;
+}
+.top {
+    flex-direction: column;
+}
+</style>
