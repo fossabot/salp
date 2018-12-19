43cb6e9abb105cd1ff5d4a4c55c4fed9db4d2dbf
Joshua Westerheide <dev@jdoubleu.de>
2018-12-19 18:25:14 +0100

    Revert "Add memory usage logging for unit tests"
    This reverts commit be645ead3cf38875db53b2e484ba01cea4a27ab4.

--- tests/unit/renderer/setup.js
+++ tests/unit/renderer/setup.js
@@ -18,12 +18,3 @@ config.mocks = {
     $store,
     $t: () => 'Mock string returned for $t function, defined in unit tests\' setup.js'
 }
-
-after('log memory usage', () => {
-    console.log('Memory usage:')
-
-    const used = process.memoryUsage()
-    for (let key in used) {
-        console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`)
-    }
-})
