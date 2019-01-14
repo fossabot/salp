26c01368306bf67dcda22f51624c3a550ee0c348
Max van Aerssen <max@vanaerssen.de>
2019-01-14 21:13:23 +0100

    Add SimpleList  component

---
+++ src/renderer/components/ContentElements/SimpleList.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'SimpleList',
+    functional: true,
+    props: {
+        ordered: {
+            type: Boolean,
+            required: true
+        }
+    },
+    render(createElement, { props, children }) {
+        return createElement(
+            props.ordered ? 'ol' : 'ul',
+            {
+                staticClass: 'contentelement-list'
+            },
+            children
+        )
+    }
+}
+</script>
+
+<style lang="scss">
+.contentelement-list {
+    margin-bottom: 0;
+}
+</style>
