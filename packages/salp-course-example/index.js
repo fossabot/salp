b33b40fbf92b7c49868d272a540f1df6d9aa704b
Joshua Westerheide <dev@jdoubleu.de>
2019-01-15 11:00:26 +0100

    Add build configuration to example course

--- packages/salp-course-example/index.js
+++ packages/salp-course-example/index.js
@@ -1,2 +1,9 @@
-console.log('Loading SQL Injections course')
-console.log('Loaded SQL Injections course')
+const chapter1 = require('./chapters/01-Introduction.md').default
+const chapter2 = require('./chapters/02-Attacks.md').default
+const chapter3 = require('./chapters/03-Mitigations.md').default
+
+module.exports = {
+    chapter1,
+    chapter2,
+    chapter3
+}
