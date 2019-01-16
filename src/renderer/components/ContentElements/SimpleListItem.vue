0325a458bcb7730947a6ed211e53ddf7806d991a
Max van Aerssen <max@vanaerssen.de>
2019-01-16 11:21:44 +0100

    Add name to SimpleListItem

--- src/renderer/components/ContentElements/SimpleListItem.vue
+++ src/renderer/components/ContentElements/SimpleListItem.vue
@@ -1,3 +1,10 @@
 <template functional>
     <li class="contentelement-listitem"><slot/></li>
 </template>
+
+<script>
+export default {
+    name: 'SimpleListItem',
+    functional: true
+}
+</script>
