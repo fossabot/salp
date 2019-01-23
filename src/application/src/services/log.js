5a2132c4f63e93da46c8683bff92d850195c547e
Joshua Westerheide <dev@jdoubleu.de>
2019-01-23 18:19:13 +0100

    Add log service unit test
    Also fix logging in production mode.

--- src/application/src/services/log.js
+++ src/application/src/services/log.js
@@ -8,6 +8,7 @@ function setup() {
         log.transports.file.level = false
         log.transports.console.level = 'debug'
     } else {
+        log.transports.file.level = 'error'
         log.transports.console.level = false
     }
 }
