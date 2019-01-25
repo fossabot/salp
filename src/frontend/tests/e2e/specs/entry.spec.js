de4d02f7c6ca459b073b39182ecf3f9079c525be
Joshua Westerheide <dev@jdoubleu.de>
2019-01-25 19:20:51 +0100

    Fix e2e stylesheet loaded tests not properly checking css rules
    By default any element has a set of css rules with browser default values. When checking with cypress one has to check their value too!

--- src/frontend/tests/e2e/specs/entry.spec.js
+++ src/frontend/tests/e2e/specs/entry.spec.js
@@ -14,37 +14,37 @@ describe('App (Entry)', () => {
     // These tests make sure the app's stylesheet is loaded
     describe('app stylesheet is loaded', () => {
         it('should have expected css rules on body', () => {
-            const expectedCssRules = [
-                'margin',
-                'padding',
-                '-webkit-app-region',
-                'font-family',
-                'background-color'
-            ]
+            const expectedCssRules = {
+                'margin': '0px',
+                'padding': '0px',
+                '-webkit-app-region': 'no-drag',
+                // 'font-family': ...,  // covered by another test
+                'background-color': 'rgb(255, 255, 255)'
+            }
 
             cy.get('body')
                 .should($el => {
-                    expectedCssRules.forEach(rule => {
-                        expect($el).to.have.css(rule)
+                    Object.entries(expectedCssRules).forEach(([rule, value]) => {
+                        expect($el).to.have.css(rule, value)
                     })
                 })
         })
 
         it('should load element-ui styles for Main component', () => {
             // see 'element-ui/packages/theme-chalk/src/main.scss'
-            const expectedCssRules = [
-                'display',
-                'flex',
-                'flex-basis',
-                'overflow',
-                'box-sizing',
-                'padding'
-            ]
+            const expectedCssRules = {
+                'display': 'block',
+                'flex': '1 1 auto',
+                'flex-basis': 'auto',
+                'overflow': 'auto',
+                'box-sizing': 'border-box'
+                // 'padding': '$--main-padding'
+            }
 
             cy.get('#app-content')
                 .should($el => {
-                    expectedCssRules.forEach(rule => {
-                        expect($el).to.have.css(rule)
+                    Object.entries(expectedCssRules).forEach(([rule, value]) => {
+                        expect($el).to.have.css(rule, value)
                     })
                 })
         })
