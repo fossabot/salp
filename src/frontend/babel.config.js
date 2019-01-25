40f8b4c06bef2d8b8f866305624755a0499fc16d
Joshua Westerheide <dev@jdoubleu.de>
2019-01-25 22:12:05 +0100

    Resolve issues when using cypress electron runner

--- src/frontend/babel.config.js
+++ src/frontend/babel.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const fileExists = require('fs').existsSync
 
 const isTesting = process.env.NODE_ENV === 'test'
+const isE2ETesting = process.env.npm_lifecycle_event && process.env.npm_lifecycle_event === 'test:e2e'
 
 const babelConfig = {
     presets: [
@@ -34,6 +35,18 @@ if (!isTesting) {
     ])
 } else {
     babelConfig.plugins.push('rewire')
+}
+
+if (isE2ETesting) {
+    babelConfig.plugins.push('@babel/plugin-transform-spread')
+    babelConfig.presets[0] = [
+        '@vue/app',
+        {
+            targets: {
+                'Electron': '1.8'
+            }
+        }
+    ]
 }
 
 module.exports = babelConfig
