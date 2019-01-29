b31e0827c0667de5b3798ef696f688041a90bfe1
Joshua Westerheide <dev@jdoubleu.de>
2019-01-29 17:25:43 +0100

    Revert "Merge branch '59-element-ui-styles-are-not-loaded-in-development-build' into 'master'"
    This reverts commit 5d111425a1edcc60c0b52c2679c3d32dcf8c5819, reversing
    changes made to 58e77a04239bb34acbd865637a6d2a093b4b2fe7.

--- src/frontend/babel.config.js
+++ src/frontend/babel.config.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const fileExists = require('fs').existsSync
+
 const isTesting = process.env.NODE_ENV === 'test'
 const isE2ETesting = process.env.npm_lifecycle_event && process.env.npm_lifecycle_event === 'test:e2e'
 
@@ -10,7 +13,27 @@ const babelConfig = {
     ]
 }
 
-if (isTesting) {
+if (!isTesting) {
+    babelConfig.plugins.push([
+    // Use forked babel plugin for component based includes
+    // @url https://github.com/ant-design/babel-plugin-import
+    // Note: After removing or adding new files to the theme, the babel-loader cache has to be flushed
+        'import',
+        {
+            'libraryName': 'element-ui',
+            'style': name => {
+                const component = name.split('/').pop()
+                let stylePath = path.resolve(__dirname, `src/theme/element/${component}.scss`)
+
+                if (!fileExists(stylePath)) {
+                    stylePath = `element-ui/packages/theme-chalk/src/${component}.scss`
+                }
+
+                return stylePath
+            }
+        }
+    ])
+} else {
     babelConfig.plugins.push('rewire')
 }
 
