0743c43f5d0db6ce2cd2b8b2b99369761f9167ff
Joshua Westerheide <dev@jdoubleu.de>
2019-02-02 18:14:57 +0100

    Add Course class of course service

---
+++ src/application/src/services/course/Course.js
@@ -0,0 +1,79 @@
+const path = require('path')
+
+class Course {
+    constructor(path) {
+        this.path = path
+
+        // hydrate info
+        this.pkgInfo = this._readPkgInfo() || {}
+        this.info = {
+            name: this.pkgInfo.name,
+            description: this.pkgInfo.description,
+            version: this.pkgInfo.version,
+            author: this.pkgInfo.author,
+            keywords: this.pkgInfo.keywords
+        }
+    }
+
+    // info getters
+    get id() {
+        return this.name
+    }
+
+    get name() {
+        return this.info.name
+    }
+
+    get description() {
+        return this.info.description
+    }
+
+    get version() {
+        return this.info.version
+    }
+
+    get author() {
+        return this.info.author
+    }
+
+    get keywords() {
+        return this.info.keywords
+    }
+
+    get sanitizedName() {
+        return this.name
+            .trim()
+            .replace(/[^a-zA-Z0-9]/gi, '_')
+    }
+
+    // methods
+    resolveContentEntry() {
+        const file = this.pkgInfo['browser']
+
+        if (!file) {
+            throw new Error('Field \'browser\' missing in package.json of course ' + this.name)
+        }
+
+        // TODO: implement resolving for external and internal bundled courses!
+        return path.join(this.path, file)
+    }
+
+    resolveBackgroundEntry() {
+        const file = this.pkgInfo['main']
+
+        if (!file) {
+            throw new Error('Field \'main\' missing in package.json of course ' + this.name)
+        }
+
+        // TODO: implement resolving for external and internal bundled courses!
+        return path.join(this.path, file)
+    }
+
+    _readPkgInfo() {
+        const pkgJson = path.join(this.path, 'package.json')
+
+        return require(pkgJson, [])
+    }
+}
+
+module.exports = Course
