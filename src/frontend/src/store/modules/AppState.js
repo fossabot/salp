7211841aa96ab0c5e101ed4bfe12203a66c6b509
Joshua Westerheide <dev@jdoubleu.de>
2019-02-06 18:14:35 +0100

    Add loaded courses properties to app state
    With mutators and getters.

--- src/frontend/src/store/modules/AppState.js
+++ src/frontend/src/store/modules/AppState.js
@@ -5,7 +5,10 @@ export const namespace = 'AppState'
 
 // types
 export const types = {
-    SET_CURRENT_COURSE: 'SET_CURRENT_COURSE'
+    SET_CURRENT_COURSE: 'SET_CURRENT_COURSE',
+    SET_LOADED_COURSES: 'SET_LOADED_COURSES',
+    GET_LOADED_COURSE: 'GET_LOADED_COURSES',
+    GET_LOADED_COURSES_IDS: 'GET_LOADED_COURSES_IDS'
 }
 
 // module
@@ -13,12 +16,25 @@ export default {
     namespaced: true,
 
     state: {
-        currentCourse: null
+        currentCourse: null,
+        loadedCourses: []
     },
 
     mutations: {
         [types.SET_CURRENT_COURSE](state, { course }) {
             state.currentCourse = course
+        },
+        [types.SET_LOADED_COURSES](state, { courses }) {
+            state.loadedCourses = courses
+        }
+    },
+
+    getters: {
+        [types.GET_LOADED_COURSE](state) {
+            return state.loadedCourses
+        },
+        [types.GET_LOADED_COURSES_IDS](state) {
+            return state.loadedCourses.map(c => c.id)
         }
     }
 }
