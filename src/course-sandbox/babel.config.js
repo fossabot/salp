5323688238e09a365ae44c5bf10888ac01b11aee
Joshua Westerheide <dev@jdoubleu.de>
2019-02-15 13:22:42 +0100

    Install element-ui in course-sandbox

--- src/course-sandbox/babel.config.js
+++ src/course-sandbox/babel.config.js
@@ -1,5 +1,35 @@
-module.exports = {
+const path = require('path')
+const fileExists = require('fs').existsSync
+
+const isTesting = process.env.NODE_ENV === 'test'
+
+const babelConfig = {
     presets: [
         '@vue/app'
-    ]
+    ],
+    plugins: []
 }
+
+if (!isTesting) {
+    babelConfig.plugins.push([
+    // Use forked babel plugin for component based includes
+    // @url https://github.com/ant-design/babel-plugin-import
+    // Note: After removing or adding new files to the theme, the babel-loader cache has to be flushed
+        'import',
+        {
+            'libraryName': 'element-ui',
+            'style': name => {
+                const component = name.split('/').pop()
+                let stylePath = path.resolve(__dirname, `src/theme/element/${component}.scss`)
+
+                if (!fileExists(stylePath)) {
+                    stylePath = `element-ui/packages/theme-chalk/src/${component}.scss`
+                }
+
+                return stylePath
+            }
+        }
+    ])
+}
+
+module.exports = babelConfig
--- src/course-sandbox/vue.config.js
+++ src/course-sandbox/vue.config.js
@@ -4,5 +4,19 @@ module.exports = {
         config.externals({
             'content.js': 'course'
         })
+
+        // Inject scss variables in each vue SFC styles
+        // @see https://cli.vuejs.org/guide/css.html#automatic-imports
+        ;['vue', 'vue-modules'].forEach(type => {
+            config.module.rule('scss').oneOf(type)
+                .use('style-resource')
+                    .loader('style-resources-loader')
+                    .options({
+                        patterns: [
+                            path.resolve(__dirname, 'src/theme/element/common/var.scss')
+                        ]
+                    })
+                    .end()
+        })
     }
 }
