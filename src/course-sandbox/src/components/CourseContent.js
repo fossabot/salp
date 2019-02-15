b8d34617be85658c193e898678dd3b4315137e81
Joshua Westerheide <dev@jdoubleu.de>
2019-02-15 16:07:44 +0100

    Load content elements in course-sandbox

--- src/course-sandbox/src/components/CourseContent.js
+++ src/course-sandbox/src/components/CourseContent.js
@@ -1,3 +1,18 @@
+import { Card } from 'element-ui'
+import Heading from './ContentElements/Heading.vue'
+import SimpleImage from './ContentElements/SimpleImage.vue'
+import AdvancedImage from './ContentElements/AdvancedImage.vue'
+import SimpleLink from './ContentElements/SimpleLink.vue'
+import SimpleText from './ContentElements/SimpleText.vue'
+import AppPreview from './ContentElements/AppPreview.vue'
+import SimpleVideo from './ContentElements/SimpleVideo.vue'
+import SimpleList from './ContentElements/SimpleList.vue'
+import SimpleListItem from './ContentElements/SimpleListItem.vue'
+import Quote from './ContentElements/Quote.vue'
+import YouTube from './ContentElements/YouTube.vue'
+import Table from './ContentElements/Table.vue'
+import InlineCode from './ContentElements/InlineCode.vue'
+
 // include actual course content
 export default async function CourseContent() {
     const contentScript = await import(
@@ -11,6 +26,23 @@ export default async function CourseContent() {
 
     return {
         name: 'CourseContent',
+        components: {
+            Heading,
+            SimpleLink,
+            SimpleText,
+            SimpleImage,
+            AdvancedImage,
+            SimpleVideo,
+            AppPreview,
+            Quote,
+            YouTube,
+            Table,
+            SimpleList,
+            SimpleListItem,
+            InlineCode,
+
+            Card
+        },
         render(createElement) {
             const chaptersElements = Object.values(chapters).map(createElement)
 
