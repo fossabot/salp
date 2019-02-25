f994d827e076b262cdf0d62c4c43dd8db8fb4bfb
Joshua Westerheide <dev@jdoubleu.de>
2019-02-25 13:21:41 +0100

    Fix course-bundler including user-script

--- packages/@salp/course-bundler/lib/injectors/user-script.js
+++ packages/@salp/course-bundler/lib/injectors/user-script.js
@@ -1,6 +1,6 @@
 module.exports = options => {
     let code = options.userScript
-    code = code ? `require('${code}');` : '/* no user script provided */'
+    code = code ? `export { default } from '${code}'` : '/* no user script provided */'
 
     return { code }
 }
