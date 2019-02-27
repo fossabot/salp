9a712b45b0fb89ea31b0ac560d54a5da7eef3299
Joshua Westerheide <dev@jdoubleu.de>
2019-02-27 18:39:52 +0100

    Add salp API mocks to course-sandbox

---
+++ src/course-sandbox/__mocks__/browser/salp/router.js
@@ -0,0 +1,15 @@
+class Router {
+    push(to) {
+        // do nothing
+    }
+
+    handleHostRouteChange(_, to) {
+        // do nothing
+    }
+
+    _registerHandlers(ipcRenderer) {
+        // do nothing
+    }
+}
+
+module.exports = new Router()
