0453aeefec3608899bf867d92d9a276be66a96ef
Joshua Westerheide <dev@jdoubleu.de>
2019-02-27 12:44:06 +0100

    Remove InitialInformation screen from page Home

--- src/frontend/src/components/Pages/Home.vue
+++ src/frontend/src/components/Pages/Home.vue
@@ -1,31 +1,22 @@
 <template>
   <div id="page-home">
-    <CoursesOverview v-if="!showInitialInformation" :groups="courseGroups"/>
-    <InitialInformation v-else />
+    <CoursesOverview :groups="courseGroups"/>
   </div>
 </template>
 
 <script>
 import CoursesOverview from '../Layout/Course/CoursesOverview.vue'
-import InitialInformation from '../Layout/Information/InitialInformation.vue'
 import { groups } from '$root/__mocks__/courses.js'
-import { namespace as userPreferencesNamespace, types as userPreferencesTypes } from '@/store/modules/persisted/UserPreferences.js'
 
 export default {
     name: 'Home',
     pageTitleTranslationKey: 'App.pages.home',
     components: {
-        CoursesOverview,
-        InitialInformation
+        CoursesOverview
     },
     data() {
         return {
             courseGroups: groups.call(this, this.$t)
-        }
-    },
-    computed: {
-        showInitialInformation() {
-            return this.$store.getters[userPreferencesNamespace + '/' + userPreferencesTypes.GET]('showInitialInformation')
         }
     }
 }
