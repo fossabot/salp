31817826f644c0bd777fb402e62f133682e47b16
Joshua Westerheide <dev@jdoubleu.de>
2019-02-28 22:30:15 +0100

    Configure development pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,24 @@ module.exports = {
         logo: '/assets/img/logo.png',
         nav: [
             { text: 'Home', link: '/' },
-            { text: 'Developer', link: '/development/' },
+            {
+                text: 'Developer',
+                link: '/development/',
+                items: [
+                    {
+                        text: 'Introduction',
+                        link: '01-Introduction.md'
+                    },
+                    {
+                        text: 'Courses',
+                        link: '02-Courses.md'
+                    },
+                    {
+                        text: 'Sandbox',
+                        link: '03-Sandbox.md'
+                    }
+                ]
+            },
             { text: 'User Manual', link: '/user-manual/' }
         ]
     }
