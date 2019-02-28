b6d8d5cf49ca65d4f93fa5078b6a2bc39046ae0b
Joshua Westerheide <dev@jdoubleu.de>
2019-02-28 22:23:09 +0100

    Configure vuepress theme

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'SALP documentation',
+    description: 'Documentation of the SALP project. Including a developer guide and user manual.',
+    themeConfig: {
+        logo: '/assets/img/logo.png',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Developer', link: '/development/' },
+            { text: 'User Manual', link: '/user-manual/' }
+        ]
+    }
+}
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,6 @@
 # SALP documentation
+![SALP logo](/assets/img/logo.png)
+
+## Index
+* [Developer](/development/)
+* [User Manual](/user-manual/)
